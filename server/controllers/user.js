86a538411a98a61fbf09d93cd12ba5142134a6d3
Ion Leahu <forza11879@gmail.com>
2021-03-17 02:50:13 -0400

    edit user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,7 @@
 import { createOrUpdateUser } from '../Models/User/index.js';
 
 export const postUser = async (req, res) => {
-  console.log('postUser from api/user routee');
+  console.log('postUser from api/user route');
 };
 
 export const getUser = async (req, res) => {
