f44a7d60a465d5869adfd5d73ccb839ec2d7d9c7
Ion Leahu <forza11879@gmail.com>
2022-06-08 10:19:02 -0400

    test

--- pages/api/cart/index.js
+++ pages/api/cart/index.js
@@ -28,3 +28,26 @@ export default async function userHandler(req, res, next) {
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
+
+async function userHandler(req, res, next) {
+  const { method } = req;
+  await db(req, res, next);
+
+  switch (method) {
+    case 'GET':
+      await authCheck(req, res, next);
+      await getUserCartController(req, res);
+      break;
+    case 'POST':
+      await authCheck(req, res, next);
+      await userCartController(req, res);
+      break;
+    case 'DELETE':
+      await authCheck(req, res, next);
+      await emptyCartController(req, res);
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST', 'DELETE']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
